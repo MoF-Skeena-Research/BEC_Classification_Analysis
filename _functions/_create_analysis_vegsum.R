4dfe5366a44116aa2cf71775182e34b7f9dcdd3b
Will MacKenzie <34073318+whmacken@users.noreply.github.com>
2022-12-30 06:42:33 -0800

    new function to reduce a veg sum file to analysis data set

---
+++ _functions/_create_analysis_vegsum.R
@@ -0,0 +1,16 @@
+##convert cover + constancy into importance value for analysis
+## apply to table built from VegdatSUsummary function
+#vegsum = vegSum
+create_analysis_vegsum <- function(vegsum, importance = 0, constancy = 0, minplots = 0){
+  vegsum <- as.data.frame(vegsum)
+  vegsum$MeanCov[vegsum$MeanCov >100] <- 100
+  vegsum$spp_importance <- vegsum$MeanCov^0.5
+  vegsum$spp_importance[vegsum$spp_importance < importance] <- NA 
+  vegsum$spp_importance[vegsum$spp_importance < 1.1] <- 0.1 
+  vegsum$spp_importance[is.na(vegsum$spp_importance)] <- 0 
+  vegsum <- vegsum %>% dplyr::mutate(spp_importance = spp_importance * (Constancy/100)) %>% dplyr::filter(spp_importance > importance)
+  ### remove species where the maximum constancy for species is < contancy
+  speciesmax <- vegsum  %>% dplyr::group_by(Species) %>% dplyr::summarise(maxcons = max(Constancy)) %>% dplyr::filter(maxcons >= constancy)
+  vegsum <- vegsum %>%  dplyr::filter(Species %in% speciesmax$Species) %>% dplyr::filter(nplots >= minplots)
+  return(vegsum)
+}
