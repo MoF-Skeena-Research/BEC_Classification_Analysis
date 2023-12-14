96818d4af0c52c15ecbae372f8ebb129c1014609
whmacken <will.mackenzie@gov.bc.ca>
2023-12-13 16:39:28 -0800

    modified to allow submission of a hierachy matrix

--- _functions/_create_su_vegdata.R
+++ _functions/_create_su_vegdata.R
@@ -1,13 +1,15 @@
- # su = SU
- # vegdata
-create_su_vegdata <- function(vegdata, su, constancycut = 0){
+ # su = hier.work
+ #  vegdata = veg.dat
+ #  unit = "Assoc"
+create_su_vegdata <- function(vegdata, su, unit = "SiteUnit", constancycut = 0){
+  su.choice <- su %>% select(SiteUnit, all_of(unit)) %>% rename(unit = 2)
   vegdat <- as.data.table(vegdata)
-  vegdat[su, SiteUnit := i.SiteUnit, on = "PlotNumber"]
+  vegdat[su, SiteUnit := i.SiteUnit, on = "PlotNumber"] ## limit data to those listed in SiteUnit
   vegdat <- vegdat[!is.na(SiteUnit) & SiteUnit != "",]
-  
   vegdat <- unique(vegdat[!is.na(SiteUnit) & SiteUnit != "",])
   vegdat3 <- vegdat[,if(.N > 1) .SD, by = .(SiteUnit,Species)]
-  vegdat3[,nplots := length(unique(PlotNumber)), by = .(SiteUnit)]
-  vegsum <- vegdat3[,.(MeanCov = sum(totalcov, na.rm = TRUE)/nplots[1], Constancy = (.N/nplots[1])*100, nplots = nplots[1]), by = .(SiteUnit,Species)]
+  vegdat4 <- vegdat3[su.choice, "unit" := unit, on = "SiteUnit"] ## swith SiteUnit to level selected
+  vegdat4[,nplots := length(unique(PlotNumber)), by = .(unit)]
+  vegsum <- vegdat4[,.(MeanCov = sum(Cover, na.rm = TRUE)/nplots[1], Constancy = (.N/nplots[1])*100, nplots = nplots[1]), by = .(unit,Species)]
   return(vegsum)
 }
