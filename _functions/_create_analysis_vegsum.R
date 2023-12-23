d1272a554cfbe74ff2a53696f1efae29887dedab
whmacken <will.mackenzie@gov.bc.ca>
2023-12-23 12:48:44 -0800

    function to create an summary dataset

--- _functions/_create_analysis_vegsum.R
+++ _functions/_create_analysis_vegsum.R
@@ -1,16 +1,16 @@
 ##convert cover + constancy into importance value for analysis
 ## apply to table built from VegdatSUsummary function
-#vegsum = veg_anal.tree; importance = 1; constancy = 60; noiseconstancy = 40; minplots = 0; covadj = .75
-create_analysis_vegsum <- function(vegsum, importance = 0, constancy = 0, noiseconstancy = 0, minplots = 0, covadj = 0.5){
+#vegsum = veg_anal.tree; minimportance = 1; minconstancy = 60; noiseconstancy = 40; minplots = 0; covadj = .75
+create_analysis_vegsum <- function(vegsum, minimportance = 0, minconstancy = 0, noiseconstancy = 0, minplots = 0, covadj = 0.5){
   vegsum <- as.data.frame(vegsum)
   vegsum$MeanCov[vegsum$MeanCov >100] <- 100
   vegsum$spp_importance <- vegsum$MeanCov^covadj
-  vegsum$spp_importance[vegsum$spp_importance < importance] <- NA 
+  vegsum$spp_importance[vegsum$spp_importance < minimportance] <- NA 
   #vegsum$spp_importance[vegsum$spp_importance < 1.1] <- 0.1 
   #vegsum$spp_importance[is.na(vegsum$spp_importance)] <- 0 
-  vegsum <- vegsum %>% dplyr::mutate(spp_importance = spp_importance * (Constancy/100)) %>% dplyr::filter(spp_importance > importance)
+  vegsum <- vegsum %>% dplyr::mutate(spp_importance = spp_importance * (Constancy/100)) %>% dplyr::filter(spp_importance > minimportance)
   ### remove species where the maximum constancy for species is < contancy
-  speciesmax <- vegsum  %>% dplyr::group_by(Species) %>% dplyr::summarise(maxcons = max(Constancy)) %>% dplyr::filter(maxcons >= constancy)
+  speciesmax <- vegsum  %>% dplyr::group_by(Species) %>% dplyr::summarise(maxcons = max(Constancy)) %>% dplyr::filter(maxcons >= minconstancy)
   vegsum <- vegsum %>%  dplyr::filter(Species %in% speciesmax$Species) %>% dplyr::filter(nplots >= minplots) %>% filter(Constancy > noiseconstancy)
   return(vegsum)
 }
