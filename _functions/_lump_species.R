de5a7b7b624d8f22ee176203a14104010afeb307
whmacken <will.mackenzie@gov.bc.ca>
2023-12-23 12:49:37 -0800

    function lumps species based on VPro lump file

--- _functions/_lump_species.R
+++ _functions/_lump_species.R
@@ -1,9 +1,13 @@
 ###Optional application of lump species
-#lumpfile = LUMP
+# lumpfile = lump
+# vegdata = veg.dat
 lump_species <- function(vegdata, lumpfile, use.subtaxa = FALSE){
 setDT(vegdata)[setDT(lumpfile), "Species" := LumpCode, on = c("Species" = "SppCode")]
  if (isFALSE(use.subtaxa)){
    vegdata$Species <-   gsub('[0-9]+', '', vegdata$Species)
  }
+  vegdata2 <- vegdata[, sum(Cover), by=list(PlotNumber,Species, Lifeform)]
+vegdata <- vegdata2 %>% dplyr::rename(Cover = V1) %>% dplyr::select(PlotNumber,Species, Cover, Lifeform)
+  
 return(vegdata)
 }
